44528a8d74d18abd0b854c71b8052d6b400e923a
pantaelaman <calvin@theophoffs.com>
2024-01-21 21:17:08 -0500

    Added line numbering package

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -9,6 +9,7 @@
 ;(package! some-package)
 
 (package! dtrt-indent)
+(package! nlinum)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
